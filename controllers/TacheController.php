392fec5f48d88ec806effc081532bced3996d4c1
Victor <herouinvictor@gmail.com>
2019-10-21 13:40:37 +0200

    cleaning folder

--- controllers/TacheController.php
+++ controllers/TacheController.php
@@ -6,10 +6,27 @@ namespace Controllers;
 
 class TacheController extends Controller
 {
-  
+       public function index($params)
+    {
+        echo "Hello ";
+        echo $params;
+    } 
     public function create($params)
     {
       $TacheMapper = spot()->mapper('Models\Tache');
       $TacheMapper->migrate();
+    }
+   public function list()
+    {
+      $TacheMapper = spot()->mapper('Models\Tache');
+      $TacheMapper->migrate();
+      $TacheList = $TacheMapper->all();
+      
+      echo $this->twig->render('list.html',
+        [
+          "tacheList" => $TacheList,
+          "quantity" => count($TacheList)
+        ]
+      );
     }
 }
